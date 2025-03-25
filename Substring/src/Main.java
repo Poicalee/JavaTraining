dfd9d2fa9de644cf8d1ffc70da411ba35690d57e
Poicalee <115011843+Poicalee@users.noreply.github.com>
2025-03-25 01:29:36 +0100

    Substrings
    Some simple exercises with substing method

---
+++ Substring/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String email ;
+        String username;
+        String domain;
+        System.out.print("Enter your email:");
+
+        email = sc.nextLine();
+        if(email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+            System.out.println(username);
+            System.out.println(domain);
+        }else{
+            System.out.println("Invalid email has to contain @");
+        }
+        
+        sc.close();
+    }
+}
