f7384edcca7ac1b8d40288d599a718eeb6e08ead
Poicalee <115011843+Poicalee@users.noreply.github.com>
2025-04-04 01:59:11 +0200

    BrainPower
    Some arrays example

---
+++ Arrays/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int [][] questions = {{3, 2},{4, 3},{4, 4},{2, 5}};
+        System.out.println(sol.mostPoints(questions));
+    }
+}
---
+++ Arrays/src/Solution.java
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int point = questions[i][0];
+            int brainpower = questions[i][1];
+            int nextIndex = i + brainpower + 1;
+
+            //solve the current question
+            long solve = point + (nextIndex < n ? dp[nextIndex] : 0);
+
+            //skip to the current question
+            long skip = dp[i + 1];
+
+            //take the maximum of both choices
+            dp[i] = Math.max(solve, skip);
+        }
+        return dp[0];
+    }
+}
